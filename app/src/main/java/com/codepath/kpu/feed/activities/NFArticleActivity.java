f166d791b0ad60bcb883174a3c58283cd169c9e3
Kenneth Pu <kp295@cornell.edu>
2016-02-14 21:17:52 -0800

    add share url functionality

--- app/src/main/java/com/codepath/kpu/feed/activities/NFArticleActivity.java
+++ app/src/main/java/com/codepath/kpu/feed/activities/NFArticleActivity.java
@@ -1,8 +1,13 @@
 package com.codepath.kpu.feed.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -15,6 +20,9 @@ public class NFArticleActivity extends AppCompatActivity {
 
     @Bind(R.id.wvArticle)
     WebView wvArticle;
+
+    private ShareActionProvider miShareAction;
+    private Intent shareIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,40 @@ public class NFArticleActivity extends AppCompatActivity {
             }
         });
         wvArticle.loadUrl(url);
+        setupShareIntent(url);
     }
 
+    private void setupShareIntent(String url) {
+        shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("text/*");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, url);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_article, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        miShareAction.setShareIntent(shareIntent);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_item_share) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
